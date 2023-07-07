743b4fb808bd3cfe8bf9c02855c92d6821494267
yuhyeonjo <jo.yuhyeon16@gmail.com>
2023-07-07 21:23:13 +0900

    회원가입 & 로그인/로그아웃 기능

---
+++ src/main/java/com/example/sbb/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.sbb.repository;
+
+import com.example.sbb.entity.SiteUser;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<SiteUser, Long> {
+    Optional<SiteUser> findByUsername(String username);
+}
---
+++ src/main/java/com/example/sbb/service/UserService.java
@@ -0,0 +1,26 @@
+package com.example.sbb.service;
+
+import com.example.sbb.entity.SiteUser;
+import com.example.sbb.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public SiteUser create(String username, String email, String password){
+        SiteUser user = new SiteUser();
+        user.setUsername(username);
+        user.setEmail(email);
+        BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        user.setPassword(passwordEncoder.encode(password));
+        this.userRepository.save(user);
+        return user;
+    }
+}
