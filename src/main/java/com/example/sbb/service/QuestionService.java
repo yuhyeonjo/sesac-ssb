8cfcf11ecdf679fcb6681fdd10207e0c23edf217
yuhyeonjo <jo.yuhyeon16@gmail.com>
2023-07-07 16:50:31 +0900

    질문 상세 페이지 생성

--- src/main/java/com/example/sbb/service/QuestionService.java
+++ src/main/java/com/example/sbb/service/QuestionService.java
@@ -1,11 +1,14 @@
 package com.example.sbb.service;
 
 import com.example.sbb.entity.Question;
+import com.example.sbb.exception.DataNotFoundException;
 import com.example.sbb.repository.QuestionRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+import com.example.sbb.exception.DataNotFoundException;
 
 @RequiredArgsConstructor
 @Service
@@ -13,7 +16,18 @@ public class QuestionService {
 
     private final QuestionRepository questionRepository;
 
+    // Question 목록 조회 
     public List<Question> getList() {
         return this.questionRepository.findAll();
+    }
+
+    // Question 데이터 조회
+    public Question getQuestion(Integer id) {
+        Optional<Question> question = this.questionRepository.findById(id);
+        if(question.isPresent()) {
+            return question.get();
+        } else {
+            throw new DataNotFoundException("question not found");
+        }
     }
 }
