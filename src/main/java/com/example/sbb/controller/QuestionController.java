f6e88b5f5c5a8849a42f9aecdfb78150370e9114
yuhyeonjo <jo.yuhyeon16@gmail.com>
2023-07-07 16:18:28 +0900

    Thymeleaf : 템플릿 설정 및 질문 리스트

--- src/main/java/com/example/sbb/controller/QuestionController.java
+++ src/main/java/com/example/sbb/controller/QuestionController.java
@@ -1,15 +1,25 @@
 package com.example.sbb.controller;
 
+import com.example.sbb.entity.Question;
+import com.example.sbb.repository.QuestionRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.List;
+
+@RequiredArgsConstructor
 @Controller
 public class QuestionController {
 
+    private final QuestionRepository questionRepository;
+
     @GetMapping("/question/list")
-    @ResponseBody
-    public String list() {
-        return "question list";
+    public String list(Model model) {
+        List<Question> questionList = this.questionRepository.findAll();
+        model.addAttribute("questionList", questionList);
+        return "question_list";
     }
 }
