0e646aad033b655903d0f5d8b3ba699c42530cd7
yuhyeonjo <jo.yuhyeon16@gmail.com>
2023-07-08 14:41:44 +0900

    질문 상세 페이지 : 답변 앵커 기능

--- src/main/java/com/example/sbb/controller/AnswerController.java
+++ src/main/java/com/example/sbb/controller/AnswerController.java
@@ -38,9 +38,8 @@ public class AnswerController {
             model.addAttribute("question", question);
             return "question_detail";
         }
-        this.answerService.create(question, answerForm.getContent(), siteUser);
-        // 답변 저장
-        return String.format("redirect:/question/detail/%s", id);
+        Answer answer = this.answerService.create(question, answerForm.getContent(), siteUser);
+        return String.format("redirect:/question/detail/%s#answer_%s", answer.getQuestion().getId(), answer.getId());
     }
 
     @PreAuthorize("isAuthenticated()") // 로그아웃 상태에서 로그인 페이지로 이동
@@ -65,7 +64,7 @@ public class AnswerController {
             throw  new ResponseStatusException(HttpStatus.BAD_REQUEST, "수정 권한이 없습니다.");
         }
         this.answerService.modify(answer, answerForm.getContent());
-        return String.format("redirect:/question/detail/%s", answer.getQuestion().getId());
+        return String.format("redirect:/question/detail/%s#answer_%s", answer.getQuestion().getId(), answer.getId());
     }
 
 
@@ -87,7 +86,7 @@ public class AnswerController {
         Answer answer = this.answerService.getAnswer(id);
         SiteUser siteUser = this.userService.getUser(principal.getName());
         this.answerService.vote(answer, siteUser);
-        return String.format("redirect:/question/detail/%s", answer.getQuestion().getId());
+        return String.format("redirect:/question/detail/%s#answer_%s", answer.getQuestion().getId(), answer.getId());
     }
 
 }
